71f8470614e2363670aa95507ecd7903c1ca7b42
Tiago França <tiagofranca.rita@gmail.com>
2024-07-02 13:41:23 -0300

    html and css form

--- helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ helpdesk/src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-tecnico-create',
   templateUrl: './tecnico-create.component.html',
   styleUrl: './tecnico-create.component.css'
 })
 export class TecnicoCreateComponent {
+  
 
 }
