78cfdb2e9fe1314152fa9eddd3a9d5cc53bf35e2
Tiago França <tiagofranca.rita@gmail.com>
2024-07-02 17:14:32 -0300

    created components update

---
+++ helpdesk/src/app/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tecnico-update',
+  templateUrl: './tecnico-update.component.html',
+  styleUrl: './tecnico-update.component.css'
+})
+export class TecnicoUpdateComponent {
+
+}
