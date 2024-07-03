d2d300d1ae31907c318add7a83d2e86a1107fc5f
Tiago França <tiagofranca.rita@gmail.com>
2024-07-03 10:26:20 -0300

    create components cliente

---
+++ helpdesk/src/app/components/cliente/cliente-delete/cliente-delete.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-delete',
+  templateUrl: './cliente-delete.component.html',
+  styleUrl: './cliente-delete.component.css'
+})
+export class ClienteDeleteComponent {
+
+}
---
+++ helpdesk/src/app/components/cliente/cliente-update/cliente-update.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cliente-update',
+  templateUrl: './cliente-update.component.html',
+  styleUrl: './cliente-update.component.css'
+})
+export class ClienteUpdateComponent {
+
+}
