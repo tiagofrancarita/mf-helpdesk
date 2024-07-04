71d2d09cbc6ff998b7c0da12259803cc6e0403ac
Tiago França <tiagofranca.rita@gmail.com>
2024-07-04 14:01:07 -0300

    ajuste chamado read

--- helpdesk/src/app/components/chamado/chamado-read/chamado-read.component.ts
+++ helpdesk/src/app/components/chamado/chamado-read/chamado-read.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Chamado } from '../../../models/chamado';
+import { ChamadoService } from '../../../services/chamado.service';
+
 
 @Component({
   selector: 'app-chamado-read',
   templateUrl: './chamado-read.component.html',
-  styleUrl: './chamado-read.component.css'
+  styleUrls: ['./chamado-read.component.css']
 })
-export class ChamadoReadComponent {
+export class ChamadoReadComponent implements OnInit {
 
+  chamado: Chamado = {
+    prioridade:  '',
+    status:      '',
+    titulo:      '',
+    observacoes: '',
+    tecnico:     '',
+    cliente:     '',
+    nomeCliente: '',
+    nomeTecnico: '',
+    descricaoChamado: ''
+  }
+
+  constructor(
+    private chamadoService: ChamadoService,
+    private toastService:    ToastrService,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.chamado.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+  }
+
+  findById(): void {
+    this.chamadoService.findById(this.chamado.id).subscribe(resposta => {
+      this.chamado = resposta;
+    }, ex => {
+      this.toastService.error(ex.error.error);
+    })
+  }
+
+  retornaStatus(status: any): string {
+    if(status == '0') {
+      return 'ABERTO'
+    } else if(status == '1') {
+      return 'EM ANDAMENTO'
+    } else {
+      return 'ENCERRADO'
+    }
+  }
+
+  retornaPrioridade(prioridade: any): string {
+    if(prioridade == '0') {
+      return 'BAIXA'
+    } else if(prioridade == '1') {
+      return 'MÉDIA'
+    } else {
+      return 'ALTA'
+    }
+  }
+  
 }
