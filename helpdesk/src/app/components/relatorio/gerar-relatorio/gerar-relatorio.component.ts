b3c3b1d44bfc3277a4ffffff9caf070a67b02054
Tiago França <tiagofranca.rita@gmail.com>
2024-07-04 14:12:18 -0300

    criacao do component relatorio

---
+++ helpdesk/src/app/components/relatorio/gerar-relatorio/gerar-relatorio.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gerar-relatorio',
+  templateUrl: './gerar-relatorio.component.html',
+  styleUrl: './gerar-relatorio.component.css'
+})
+export class GerarRelatorioComponent {
+
+}
